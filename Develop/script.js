4edeb67780fd1b9b680b7b38ed1b6629ed023d5f
Acoleman37 <aleccoleman123@yahoo.com>
2021-11-05 20:54:45 -0700

    moved the file structure around

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 
 // Various Arrays 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!@#$%^&*(){/}<>,.?~"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -18,7 +18,7 @@ function generatePassword() {
   var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
   // Loop if answer is outside the parameters 
-  while(confirmLength <= 7 || confirmLength >= 128) {
+  if(confirmLength <= 7 || confirmLength >= 128) {
       alert("Password length must be between 8-128 characters Try again");
       var confirmLength = (prompt("How many characters would you like your password to contain?"));
       } 
@@ -32,7 +32,7 @@ function generatePassword() {
     var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
     var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
       // Loop if answer is outside the parameters 
-      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+      if(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
         alert("You must choose at least one parameter");
         var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
         var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
