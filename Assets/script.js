faad083c2968d88f1a0018f706d3456728670339
Acoleman37 <aleccoleman123@yahoo.com>
2021-11-05 21:11:01 -0700

    .

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,3 @@
-// Assignment code here
-
 // Various Arrays 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
